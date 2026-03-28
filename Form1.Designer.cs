35ad1ee1fe928704206e5b02360c3cafd7078b23
Enukar <Pabloraho@gmail.com>
2026-03-28 18:14:19 -0300

    Inicio del projecto

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.boton1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // boton1
+            // 
+            this.boton1.BackColor = System.Drawing.Color.Crimson;
+            this.boton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.boton1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.boton1.Location = new System.Drawing.Point(57, 100);
+            this.boton1.Name = "boton1";
+            this.boton1.Size = new System.Drawing.Size(75, 23);
+            this.boton1.TabIndex = 0;
+            this.boton1.Text = "botoncito";
+            this.boton1.UseVisualStyleBackColor = false;
+            this.boton1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(144, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(525, 66);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Never stop learning!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boton1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hello World!";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button boton1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,53 @@ namespace Programacion_3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenidos a C#");
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Chau Chau ...");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs click = (MouseEventArgs)e;
+
+            if (click.Button == MouseButtons.Left)
+                MessageBox.Show("Presiono el boton izquierdo", "Atencion!");
+            else if (click.Button == MouseButtons.Right)
+                MessageBox.Show("Presiono el boton derecho", "Atencion!");
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs click = (MouseEventArgs)e;
+            if (click.Button == MouseButtons.Left)
+                MessageBox.Show("Presiono el botón Izquierdo", "Atención");
+            else if (click.Button == MouseButtons.Right)
+                MessageBox.Show("Presiono el Botón Derecho", "Atención");
+            else if (click.Button == MouseButtons.Middle)
+                MessageBox.Show("Presiono el botón del Medio", "Atención");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.BackColor = Color.Cyan;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Black;
+        }
     }
 }
